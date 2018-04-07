2d1b5fd1abb652ad192544699dbe20d5ee969ec0
li.yiran <li.yiran@huadongmedia.com>
2018-04-08 02:04:53 +0800

    hello springmvc

---
+++ src/main/java/com/eran/HelloMvcController.java
@@ -0,0 +1,15 @@
+package com.eran;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloMvcController {
+	
+	@RequestMapping("/mvc")
+	public String helloMvc(){
+		return "home";
+	}
+
+}
